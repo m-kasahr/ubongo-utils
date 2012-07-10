e2996e26a6f7e06688352cbde6f06cf973ca5f62
Motoyuki Kasahara <m-kasahr.sbc2@live.jp>
2012-07-10 20:55:21 +0900

    Rename Ubongo::PieceSet_Readir to Ubongo::PieceSet_Dir.

--- lib/ubongo/pieceset_dir.h
+++ lib/ubongo/pieceset_dir.h
@@ -1,23 +1,23 @@
-#ifndef UBONGO_PIECESET_READDIR_H
-#define UBONGO_PIECESET_READDIR_H
+#ifndef UBONGO_PIECESET_DIR_H
+#define UBONGO_PIECESET_DIR_H
 
 #include <string>
 #include "ubongo/pieceset.h"
 
 namespace Ubongo {
-    class PieceSet_ReadDir : public PieceSet {
+    class PieceSet_Dir : public PieceSet {
 	void
 	scan_dir(const char *path, std::vector<Piece> &pieces,
 	    bool &piece_flip_flag);
 
-	PieceSet_ReadDir();
+	PieceSet_Dir();
 
     public:
 	static const size_t max_line_length = 256;
 
-        PieceSet_ReadDir(const char *path);
-        PieceSet_ReadDir(const std::string &path);
+        PieceSet_Dir(const char *path);
+        PieceSet_Dir(const std::string &path);
     };
 }
 
-#endif // UBONGO_PIECESET_READDIR_HPP
+#endif // UBONGO_PIECESET_DIR_HPP
